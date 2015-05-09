2206fc7453f7078a1dc47559448f684b25b0e0c8
Q-L <qiuyueliu97@gmail.com>
2015-05-08 23:04:25 -0700

    one major bug fixed(yes I forgot to upload the GoToPageCode or to include it as one of the scripts), and some other minor work

---
+++ SecretPigeonGoToPageCode.js
@@ -0,0 +1,11 @@
+function goToTutorial(){
+	window.location.href = "#tutorialPage1";
+}
+
+function goToSetup() {
+	window.location.href = "#firstSetupPage2"; 
+}
+
+function goToSetup3_2(){
+	window.location.href = "#firstSetupPage3-2"
+}
--- SecretPigeonSettingsCode.js
+++ SecretPigeonSettingsCode.js
@@ -1,20 +1,26 @@
+var termAgreed = false;//We don't really need this variable this split moment...but I believe it will become helpful in the future
+
+var buttonChoiceForSetup3_2 = "";
+
 function checkTermAndServiceCheckbox(){
 	if($('#termAndService').prop('checked')) {
 		$('#nameSetupPageNextButton').prop('disabled', false);
+		termAgreed = true;
 	}
 	else{
 		$('#nameSetupPageNextButton').prop('disabled', true);
+		termAgreed = false;
 	}
 }
 
-function goToTutorial(){
-	window.location.href = "#tutorialPage1";
+function storeButtonChoice(buttonChoice){
+	buttonChoiceForSetup3_2 = buttonChoice;
 }
 
-function goToSetup() {
-	window.location.href = "#firstSetupPage2"; 
+function changefirstSetupPage3_2ButtonName(){
+	var start = "Start personalizing ";
+	var end = " button";
+	
+	$('#firstSetupPage3-2Title').text(start.concat(buttonChoiceForSetup3_2.concat(end)));
 }
 
-function goToSetup3_2(){
-	window.location.href = "#firstSetupPage3-2"
-}
