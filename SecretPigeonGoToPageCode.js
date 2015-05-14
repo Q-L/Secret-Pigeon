7a26f87bba7ba2e52beb76acca3f881436808bdf
Q-L <qiuyueliu97@gmail.com>
2015-05-13 19:52:28 -0700

    just a commit

--- SecretPigeonGoToPageCode.js
+++ SecretPigeonGoToPageCode.js
@@ -7,5 +7,17 @@ function goToSetup() {
 }
 
 function goToSetup3_2(){
-	window.location.href = "#firstSetupPage3-2"
+	window.location.href = "#firstSetupPage3-2";
+}
+
+function goToSettingsMessage1(){
+	window.location.href = "#settingsMessage1";
+}
+
+function goToSettingsMessageNewScenario(){
+	window.location.href = "#settingsMessageNewScenario";
+}
+
+function goBack(){
+    window.history.back();
 }
